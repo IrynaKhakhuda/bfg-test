fb13bcd632bf1627c1c4e41ec1206adc4dce994e
Iryna_Khakhuda <iryna_khakhuda@epam.com>
2023-01-05 15:06:15 +0200

    Initial commit

---
+++ src/main/java/com/example/bfgtest/BfgTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bfgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfgTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BfgTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bfgtest/BfgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bfgtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfgTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
